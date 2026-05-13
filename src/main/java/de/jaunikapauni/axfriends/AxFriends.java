d8c7d07fff2c59826b68e80062ee74c74eb18fa8
JauniKapauni <riches-goods.54@icloud.com>
2026-05-13 19:43:45 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axfriends/AxFriends.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axfriends;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxFriends extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
