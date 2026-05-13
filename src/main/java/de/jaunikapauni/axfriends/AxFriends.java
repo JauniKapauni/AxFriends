d9f1f826cf423992023f2b48495713ccd3d62b99
JauniKapauni <riches-goods.54@icloud.com>
2026-05-13 19:51:46 +0200

    feat: add db logic and table initialization

--- src/main/java/de/jaunikapauni/axfriends/AxFriends.java
+++ src/main/java/de/jaunikapauni/axfriends/AxFriends.java
@@ -1,12 +1,28 @@
 package de.jaunikapauni.axfriends;
 
+import de.jaunikapauni.axfriends.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxFriends extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() && databaseManager.initDatabaseTable2() == false){
+                getLogger().severe("Error creating tables!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
