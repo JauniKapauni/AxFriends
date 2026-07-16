79151ddb1260d343e7b8d68269f1d7284e894ba9
JauniKapauni <riches-goods.54@icloud.com>
2026-07-16 23:49:00 +0200

    fix(tabcompleter): return actual names

--- src/main/java/de/jaunikapauni/axfriends/command/RemoveFriendTabCompleter.java
+++ src/main/java/de/jaunikapauni/axfriends/command/RemoveFriendTabCompleter.java
@@ -1,6 +1,8 @@
 package de.jaunikapauni.axfriends.command;
 
 import de.jaunikapauni.axfriends.AxFriends;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -10,21 +12,32 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class RemoveFriendTabCompleter implements TabCompleter {
     AxFriends reference;
-    public RemoveFriendTabCompleter(AxFriends reference){
+
+    public RemoveFriendTabCompleter(AxFriends reference) {
         this.reference = reference;
     }
+
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if(!(sender instanceof Player)){
+        if (!(sender instanceof Player)) {
             return List.of();
         }
-        Player p = (Player) sender;
-        if(args.length == 1){
-            return reference.getPlayerManager().listFriends(p);
+        if (args.length == 1) {
+            return List.of();
         }
-        return List.of();
+        List<String> playerNames = new ArrayList<>();
+        Player p = (Player) sender;
+
+        for (String uuid : reference.getPlayerManager().listFriends(p)) {
+            OfflinePlayer player = Bukkit.getOfflinePlayer(UUID.fromString(uuid));
+            if (player.getName() != null) {
+                playerNames.add(player.getName());
+            }
+        }
+        return playerNames;
     }
 }
